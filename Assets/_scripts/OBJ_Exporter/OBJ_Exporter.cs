1f9072ae43cf8e5e2ee5621d0730e9c4313c2cfa
Azizhan Çil <azizhancil23@gmail.com>
2025-03-24 15:41:59 +0300

    summary added

--- Assets/_scripts/OBJ_Exporter/OBJ_Exporter.cs
+++ Assets/_scripts/OBJ_Exporter/OBJ_Exporter.cs
@@ -5,7 +5,13 @@ using FactoryTemplate.Machines;
 using Unity.VisualScripting;
 using UnityEngine;
 
-//That is part of my biggest project you can make this code more abstract and use it in your project	
+
+/// <summary>
+/// Utility class for exporting Unity GameObject hierarchies to OBJ format, handling mesh filtering,
+/// transformation to world space, and preserving object structure through named groups.
+/// That is part of my biggest project you can make this code more abstract and use it in your project
+/// </summary>
+
 public static class OBJExporter
 {
 	public static void Export(GameObject parent, string filePath, string nameOfExport)
